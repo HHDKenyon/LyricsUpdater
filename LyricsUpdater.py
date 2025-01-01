844946e6f39a677e6aad6eed4077ad8fa4fb703b
HHDKenyon <48248306+HHDKenyon@users.noreply.github.com>
2025-01-01 22:01:15 +0000

    Added support for .wma files

--- LyricsUpdater.py
+++ LyricsUpdater.py
@@ -4,6 +4,7 @@ from mutagen.easyid3 import EasyID3
 from mutagen.id3 import ID3
 from mutagen.id3._frames import USLT
 from mutagen.flac import FLAC
+from mutagen.asf import ASF
 
 # Initialize Genius API
 genius = lyricsgenius.Genius("M05OffvrQaFmzlTd4Bnbs1gghydd7cv7ziO-fzu4adev4tyQ-7NbXfNSTvFme55a")
@@ -28,24 +29,31 @@ def embed_lyrics(file_path, lyrics, file_type):
         audio = FLAC(file_path)
         audio["lyrics"] = lyrics
         audio.save()
+    elif file_type == 'wma':
+        audio = ASF(file_path)
+        audio["WM/Lyrics"] = lyrics
+        audio.save()
 
 def process_folder(folder_path, overwrite):
     for root, _, files in os.walk(folder_path):
         for file in files:
-            if file.endswith((".mp3", ".flac")):
+            if file.endswith((".mp3", ".flac", ".wma")):
                 file_path = os.path.join(root, file)
-                file_type = 'mp3' if file.endswith(".mp3") else 'flac'
-                
-                if file_type == 'mp3':
+                if file.endswith(".mp3"):
+                    file_type = 'mp3'
                     audio = EasyID3(file_path)
-                elif file_type == 'flac':
+                elif file.endswith(".flac"):
+                    file_type = 'flac'
                     audio = FLAC(file_path)
+                elif file.endswith(".wma"):
+                    file_type = 'wma'
+                    audio = ASF(file_path)
                 
                 song_title = audio.get("title", [None])
                 song_title = song_title[0] if song_title else None
                 artist = audio.get("artist", [None])
                 artist = artist[0] if artist else None
-                existing_lyrics = audio.get("lyrics", None) if file_type == 'flac' else audio.get("USLT", None)
+                existing_lyrics = audio.get("lyrics", None) if file_type == 'flac' else audio.get("USLT", None) if file_type == 'mp3' else audio.get("WM/Lyrics", None)
 
                 if song_title and artist:
                     if existing_lyrics and not overwrite:
